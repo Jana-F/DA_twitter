6ca0cacaea7642186886544a17fe74486a321691
Jana-F <jana@fedra.cz>
2017-07-05 16:20:28 +0200

    Update project.py

--- project.py
+++ project.py
@@ -3,5 +3,5 @@ from twitter import download_followers, count_followers
 
 if __name__ == '__main__':
     #download_followers('yedpodtrzitko')
-    followers_week = count_followers('yedpodtrzitko', '2017-06-30', '2017-07-05')
-    render_graph(followers_week)
+    followers_date_and_sum = count_followers('yedpodtrzitko', '2017-06-30', '2017-07-05')
+    render_graph(followers_date_and_sum)
